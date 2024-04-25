9eafa8957cd31bb98b6185e6ec6aa5e82ea902d5
akiran1sei <nakamori.1224@gmail.com>
2024-04-25 14:10:42 +0900

    データベース先変更

--- src/app/utils/database.js
+++ src/app/utils/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const USER_NAME = "akiran1sei";
 const USER_PASSWD = "akiran1sei";
-const HOST_NAME = "cluster0.vgefpwi.mongodb.net";
+const HOST_NAME = "cluster0.rp1u5ye.mongodb.net";
 const DB_NAME = "test";
 const uri = `mongodb+srv://${USER_NAME}:${USER_PASSWD}@${HOST_NAME}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
