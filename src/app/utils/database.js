4daa53579a3be9609ec52792ebbac1f195a97640
akiran1sei <nakamori.1224@gmail.com>
2024-04-25 14:22:28 +0900

    編集

--- src/app/utils/database.js
+++ src/app/utils/database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-const USER_NAME = "akiran1sei";
-const USER_PASSWD = "akiran1sei";
-const HOST_NAME = "cluster0.rp1u5ye.mongodb.net";
-const DB_NAME = "test";
-const uri = `mongodb+srv://${USER_NAME}:${USER_PASSWD}@${HOST_NAME}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+// const USER_NAME = "akiran1sei";
+// const USER_PASSWD = "akiran1sei";
+// const HOST_NAME = "cluster0.rp1u5ye.mongodb.net";
+// const DB_NAME = "test";
+const uri = `mongodb+srv://akiran1sei:akiran1sei@cluster0.rp1u5ye.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${USER_NAME}:${USER_PASSWD}@${HOST_NAME}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
 const connectDB = async () => {
   try {
