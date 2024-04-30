e62cfd45e9fd0a228ca6a86c63c275172bda1f78
akiran1sei <nakamori.1224@gmail.com>
2024-04-30 10:34:48 +0900

    修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-// export default nextConfig;
-module.exports = {
-  cacheHandler: require.resolve("./cache-handler.js"),
-  cacheMaxMemorySize: 0, // disable default in-memory caching
-};
+export default nextConfig;
