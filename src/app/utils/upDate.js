8c9853403d5519075afdb5add38a6ec387f60e81
akiran1sei <nakamori.1224@gmail.com>
2024-04-30 11:04:39 +0900

    修正

--- src/app/utils/upDate.js
+++ src/app/utils/upDate.js
@@ -1,8 +1,9 @@
 import { revalidatePath } from "next/cache";
-
+import dotenv from "dotenv";
 const upDate = async (req, res) => {
-  // console.log("upData:", req);
-  revalidatePath(`/pages/group`);
+  dotenv.config();
+
+  revalidatePath(`${process.env.NEXT_PUBLIC_URL}/pages/group`);
   return req;
 };
 export default upDate;
